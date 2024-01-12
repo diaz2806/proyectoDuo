69026735a66a101c522f912153323e5f415b7d6b
diaz2806 <alb.fiaz.esq@gmail.com>
2024-01-12 10:37:58 +0100

    Subida cambio nombre proyecto java

--- proyectoDuo/src/HolaDAM/HolaDAM.java
+++ proyectoDuo/src/HolaDAM/HolaDAM.java
@@ -2,19 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package proyectoduo;
+package HolaDAM;
 
 /**
  *
  * @author diaesqal
  */
-public class ProyectoDuo {
+public class HolaDAM {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola DAM");
     }
     
 }
