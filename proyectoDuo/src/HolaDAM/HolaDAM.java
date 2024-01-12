1ea37100aab40a6f85eef3b14fedb7d431a723c1
maytompa <pablomayortomillo@gmail.com>
2024-01-12 10:44:58 +0100

    Subida vacana

--- proyectoDuo/src/HolaDAM/HolaDAM.java
+++ proyectoDuo/src/HolaDAM/HolaDAM.java
@@ -16,6 +16,8 @@ public class HolaDAM {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola DAM");
+        int a=15;
+        System.out.println(a);
     }
     
 }
